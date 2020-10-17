a26b4d75bded7a41751ff679284a34c379797d77
Manas Mishra <er>
2020-10-17 15:27:25 +0530

    Added function.h and Codes folder

---
+++ Codes/driver.c
@@ -0,0 +1,6 @@
+int main(){
+    // TODO Create Grammar from grammar.txt
+    // TODO  Code from source.txt
+    // TODO Create ParseTree using Code and Grammar
+    // TODO Print parse tree
+}
---
+++ function.h
@@ -0,0 +1,16 @@
+// https://github.com/UtkarshPathrabe/Compiler-Construction-BITS-Pilani/blob/master/Compiler%20Project/Language%20Details.pdf
+
+// ******** HEADER FILES ********
+// tree.h
+// SymbolTable.h 
+// LinkedList.h
+//SymbolLinkedList.h
+
+// ********** FUNCTIONS IN ASSIGNMENT **********
+// readGrammar( “grammar.txt”, grammar G)
+// tokeniseSourcecode( “sourcecode.txt”, tokenStream *s)
+// createParseTree( parseTree *t, tokenStream *s, grammar G)
+// traverseParseTree(parseTree *t, typeExpressionTable T )
+// printParseTree(parseTree *t)
+// printTypeExpressionTable (typeExpressionTable T)
+// Driver function()
