535143db47ab64e394533975fb0aeca9d7fcc07b
Manas Mishra <er>
2020-10-17 15:28:47 +0530

    Updated function.h

--- function.h
+++ function.h
@@ -1,11 +1,3 @@
-// https://github.com/UtkarshPathrabe/Compiler-Construction-BITS-Pilani/blob/master/Compiler%20Project/Language%20Details.pdf
-
-// ******** HEADER FILES ********
-// tree.h
-// SymbolTable.h 
-// LinkedList.h
-//SymbolLinkedList.h
-
 // ********** FUNCTIONS IN ASSIGNMENT **********
 // readGrammar( “grammar.txt”, grammar G)
 // tokeniseSourcecode( “sourcecode.txt”, tokenStream *s)
@@ -14,3 +6,34 @@
 // printParseTree(parseTree *t)
 // printTypeExpressionTable (typeExpressionTable T)
 // Driver function()
+
+//######## 1st ########
+// https://github.com/UtkarshPathrabe/Compiler-Construction-BITS-Pilani/blob/master/Compiler%20Project/Language%20Details.pdf
+
+// ******** HEADER FILES ********
+// tree.h
+// SymbolTable.h 
+// LinkedList.h
+//SymbolLinkedList.h
+
+//######## 2nd ######## 😎😎😎😎😎😎
+// Most readable and similar by far.
+//https://github.com/Shivankit-Gaind/Compiler-Project
+// ******** HEADER FILES ********
+//NaryTree.h
+//lookUpTable.h
+//symbolTable.h
+//lexer.h
+
+//######## 3rd ########
+// Language from grammar
+// https://github.com/SinghCoder/Compiler-Construction/blob/master/grammar_from_mam.pdf
+//grammar_from_mam.pdf
+
+//######## 4th ########
+///https://github.com/likecs/Compiler-ERPLAG/tree/master/Complete%20Project
+//many test cases
+
+///######## 5th ########
+//https://github.com/Aashish683/Compiler-Project/blob/master/Compiler/printer.h
+//print cases files
